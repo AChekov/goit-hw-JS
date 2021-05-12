9dd7629c4887c2e67a29a058d38593eab7f4c87a
AChekov <4ekoff@gmail.com>
2021-05-12 22:55:56 +0300

    копии дз 6/10

---
+++ src/index.js
@@ -0,0 +1,66 @@
+// ============задача 6/36============
+// Магазин по продаже ремонтных дроидов готов к открытию, осталось написать скрипт для их заказа. Объяви переменные и присвой им соответствующие значения:
+// pricePerDroid - цена одного дроида, значение 800
+// orderedQuantity - количество дроидов в заказе, значение 6
+// deliveryFee - стоимость доставки, значение 50
+// totalPrice - общая сумма заказа к оплате, не забудь о стоимости доставки
+// message - сообщение о состоянии заказа в формате "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price."
+
+// Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// ============задача 7/36============
+// Объяви функцию sayHi, внутри которой добавь console.log() со строкой "Hello, this is my first function!". После объявления вызови функцию sayHi.
+
+// Change code below this line
+// function sayHi() {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi();
+
+// ============Задача 8 / 36============
+// Функция add должна уметь складывать три числа и выводить результат в консоль. Добавь функции add три параметра a, b и c, которые будут получать значения аргументов при её вызове.
+// Дополни console.log() так, чтобы он логировал строку "Addition result equals <result>", где <result> это сумма переданных чисел.
+
+// Change code below this line
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+//============Задача 9 / 36============
+//Дополни код функции add так, чтобы она возвращала результат сложения значений трёх параметров a, b и c.
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+//   return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+//============Задача 10 / 36============
+//Дополни код функции так, чтобы в переменную message записывалась строка
+//"You picked <product name>, price per item is <product price> credits", где <product name> и <product price> это значения параметров name и price. Используй синтаксис шаблонных строк.
+
+// function makeMessage(name, price) {
+//   // Change code below this line
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// }
