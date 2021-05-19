6e03522930269bd311df82a5f3b4d84fa5e15b1c
AChekov <4ekoff@gmail.com>
2021-05-19 23:55:53 +0300

    partModul2

---
+++ src/modul2.js
@@ -0,0 +1,131 @@
+//============Задача 11 / 32============
+// Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
+// Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
+// Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//     // Change code below this line
+
+//  message = message.split(" ").length;
+//  const price = message * pricePerWord;
+
+//    return price;
+//     // Change code above this line
+//  }
+
+//============Задача 12 / 32============
+// Дополни код функции makeStringFromArray(array, delimeter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimeter - строку.
+// function makeStringFromArray(array, delimeter) {
+//     let string;
+//     // Change code below this line
+
+//   string = array.join(delimeter);
+
+//     // Change code above this line
+//     return string;
+//   }
+
+//============Задача 13 / 32============
+// Термин slug - это человеко-понятный уникальный идентификатор, который используется в веб-разработке для создания читабельных URL-адесов.
+
+// Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx, можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+
+// Внимание
+// Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+// Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки.
+
+// Значением параметра title будут строки, слова которых разделены только пробелами
+// Все символы slug должны быть в нижнем регистре
+// Все слова slug должна быть разделены тире
+// Тесты
+// Объявлена функция slugify(title)
+// Вызов slugify("Arrays for begginers") возвращает "arrays-for-begginers"
+// Вызов slugify("English for developer") возвращает "english-for-developer"
+// Вызов slugify("Ten secrets of JavaScript") возвращает "ten-secrets-of-javascript"
+// Вызов slugify("How to become a JUNIOR developer in TWO WEEKS") возвращает "how-to-become-a-junior-developer-in-two-weeks"
+
+// function slugify(title) {
+//     // Change code below this line
+
+//   const slug=title.toLowerCase().split(' ' ).join('-');
+//     return slug;
+//     // Change code above this line
+//   }
+
+//============Задача 14 / 32============
+// Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits.
+
+// firstTwoEls - массив из первых двух элементов
+// nonExtremeEls - массив из всех элементов кроме первого и последнего
+// lastThreeEls - массив из трёх последних элементов
+// Тесты
+// Объявлена переменная fruits
+// Значение переменной fruits это массив ["apple", "plum", "pear", "orange", "banana"]
+// Объявлена переменная firstTwoEls
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, -1);
+// const lastThreeEls = fruits.slice(-3);
+
+//============Задача 16 / 32============
+// Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray. Параметр maxLength содержит максимально допустимую длину нового массива.
+
+// Если количество элементов нового массива больше maxLength, функция должна вернуть копию массива длиной maxLength элементов. В противном случае функция должна вернуть новый массив целиком.
+
+// Тесты
+// Объявлена функция makeArray(firstArray, secondArray, maxLength)
+// Вызов makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3) возвращает ["Mango", "Poly", "Ajax"]
+// Вызов makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4) возвращает ["Mango", "Poly", "Houston", "Ajax"]
+// Вызов makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3) возвращает ["Mango", "Ajax", "Chelsea"]
+// Вызов makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2) возвращает ["Earth", "Jupiter"]
+// Вызов makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4) возвращает ["Earth", "Jupiter", "Neptune", "Uranus"]
+// Вызов makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0) возвращает []
+// Вызов функции makeArray() со случайными массивами и случайным числом возвращает правильный массив
+
+//=====первый способ
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+
+// let newArray = firstArray.concat(secondArray);
+//   if (newArray.length > maxLength) {
+//   newArray.length = maxLength;
+//     return newArray;
+//   }
+// return newArray.slice(0,);
+//     // Change code above this line
+//   }
+
+//=====второй способ (не полный)
+// let newArray = firstArray.concat(secondArray.slice(0, maxLength));
+
+// return newArray;
+
+//============Задача 18 / 32============
+// Напиши функцию calculateTotal(number), которая принимает целое число (параметр number) и возвращает сумму всех целых чисел от единицы и до этого числа. Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+
+// Тесты
+// Объявлена функция calculateTotal(number)
+// Вызов функции calculateTotal(1) возвращает 1
+// Вызов функции calculateTotal(3) возвращает 6
+// Вызов функции calculateTotal(7) возвращает 28
+// Вызов функции calculateTotal(18) возвращает 171
+// Вызов функции calculateTotal(24) возвращает 300
+// Вызов функции calculateTotal() со случайным числом возвращает правильное значение
+
+// function calculateTotal(number) {
+//     // Change code below this line
+
+//    let calc = 0;
+//    for (i = 1; i <= number; i += 1) {
+//    calc += i;
+//    }
+//    return calc;
+
+//      // Change code above this line
+//    }
+
+//============Задача 19 / 32============
