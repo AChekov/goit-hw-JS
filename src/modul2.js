ad438b32b16ebac6736ddc1a694dbf4b33ba1550
AChekov <4ekoff@gmail.com>
2021-05-24 00:09:25 +0300

    Update module 2 JS (the end)

--- src/modul2.js
+++ src/modul2.js
@@ -177,3 +177,357 @@
 // }
 
 // console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+
+// Задача: фильтрация массива чисел
+// Задание
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел (параметр numbers) и возвращает новый массив, в котором будут только те элементы массива numbers, которые больше чем значение параметра value (число).
+
+//============Задача 23 / 32============
+// Тесты
+// Объявлена функция filterArray(numbers, value)
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+// Вызов функции filterArray() со случайным массивом и числом возвращает правильный массив
+// В цикле for использовался метод push
+
+// function filterArray(numbers, value) {
+//     // Change code below this line
+//    const result = [];
+//  for(let i = 0; i < numbers.length; i += 1){
+//    if (numbers[i] > value){
+//      result.push(numbers[i]);
+//    }
+
+//  }
+//  return result;
+
+//    // Change code above this line
+//  }
+
+//============Задача 24 / 32============
+// Метод includes()
+// Метод includes(value) проверяет есть ли в массиве элемент со значением value и возвращает true или false соответственно. Область применения этого метода сводится к ситуациям, когда необходимо проверить есть ли элемент в массиве и не важна его позиция (индекс).
+
+// const planets = ["Earth", "Mars", "Venus"];
+
+// console.log(planets.includes("Earth")); // true
+// console.log(planets.includes("Mars")); // true
+// console.log(planets.includes("Venus")); // true
+// console.log(planets.includes("Jupiter")); // false
+// Задание
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+
+// Дополни код функции так, что если:
+
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+// Тесты
+// Объявлена функция checkFruit(fruit)
+// Вызов checkFruit("plum") возвращает true
+// Вызов checkFruit("mandarin") возвращает false
+// Вызов checkFruit("pear") возвращает true
+// Вызов checkFruit("Pear") возвращает false
+// Вызов checkFruit("apple") возвращает true
+// Вызов функции checkFruit() со случайным словом возвращает правильное значение boolean
+// В функции использовался метод includes
+
+// function checkFruit(fruit) {
+//     const fruits = ["apple", "plum", "pear", "orange"];
+
+//     return fruits.includes(fruit); // Change this line
+//   }
+
+//============Задача 25 / 32============
+
+// Задача: общие элементы
+// Задание
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+
+// Например, в двух массивах [1, 3, 5] и [0, 8, 5, 3] общими будут числа 3 и 5, т.к. они присутствуют в обоих исходных массивах. А числа 0, 1 и 8 присутствуют только в одном из массивов.
+
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2, и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+
+// Тесты
+// Объявлена функция getCommonElements(array1, array2)
+// Вызов getCommonElements([1, 2, 3], [2, 4]) возвращает [2]
+// Вызов getCommonElements([1, 2, 3], [2, 1, 17, 19]) возвращает [1, 2]
+// Вызов getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) возвращает [12, 27, 3]
+// Вызов getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) возвращает [10, 30, 40]
+// Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает []
+// Вызов функции getCommonElements() со случайными двумя массивами возвращает правильный массив
+// В цикле for использовались методы includes и push
+
+// function getCommonElements(array1, array2) {
+//     // Change code below this line
+//     const uniqieElements = [];
+//   for(const element of array1){
+//     if(array2.includes(element)){
+//       uniqieElements.push(element);
+//     }
+//   }
+//     return uniqieElements;
+
+//    // Change code above this line
+//   }
+
+//============Задача 26 / 32============
+// Цикл for...of
+// Инструкция for...of объявляет цикл, перебирающий итерируемые объекты, такие как массивы и строки. Тело цикла будет выполняться для значения каждого элемента. Это хорошая замена циклу for, если не нужен доступ к счётчику итерации.
+
+// for (const variable of iterable) {
+//   // тело цикла
+// }
+// variable — переменная, которая будет хранить значение элемента на каждой итерации
+// iterable — коллекция, которая имеет перечислимые элементы, например массив
+// const planets = ["Earth", "Mars", "Venus"];
+
+// for (const planet of planets) {
+//   console.log(planet);
+// }
+// Задание
+// Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.
+
+// Тесты
+// Объявлена функция calculateTotalPrice(order)
+// Вызов функции calculateTotalPrice([12, 85, 37, 4]) возвращает 138
+// Вызов функции calculateTotalPrice([164, 48, 291]) возвращает 503
+// Вызов функции calculateTotalPrice([412, 371, 94, 63, 176]) возвращает 1116
+// Вызов функции calculateTotalPrice([]) возвращает 0
+// Вызов функции calculateTotalPrice() со случайным массивом чисел возвращает правильную сумму
+
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     // Change code below this line
+
+//     for (const item of order) {
+//       total += item;
+//     }
+
+//     // Change code above this line
+//     return total;
+//   }
+
+//============Задача 29 / 32============
+// Задача: чётные числа
+// Задание
+// Напиши функцию getEvenNumbers(start, end) которая возвращает массив всех чётных чисел от start до end. Чётным считается число которое делится на 2 без остатка (10 % 2 === 0).
+
+// Тесты
+// Объявлена функция getEvenNumbers(start, end)
+// Вызов функции getEvenNumbers(2, 5) возвращает [2, 4]
+// Вызов функции getEvenNumbers(3, 11) возвращает [4, 6, 8, 10]
+// Вызов функции getEvenNumbers(6, 12) возвращает [6, 8, 10, 12]
+// Вызов функции getEvenNumbers(8, 8) возвращает [8]
+// Вызов функции getEvenNumbers(7, 7) возвращает []
+// Вызов функции getEvenNumbers() со случайными start и end возвращает правильный массив
+
+// function getEvenNumbers(start, end) {
+//     // Change code below this line
+//  const number = [];
+//      for (i = start; i <= end; i += 1){
+//          if (i % 2 === 0){
+//           number.push(i);
+//          }
+//      }
+//    return number;
+//      // Change code above this line
+//    }
+
+//============Задача 30 / 32============
+// Оператор break
+// Прервать выполнение цикла можно в любой момент. Для этого существует оператор break, который полностью прекращает выполнение цикла и передаёт управление на строку за его телом.
+
+// В примере ищем число 3. Как только выполнится условие if, цикл прекратит своё выполнение (будет прерван).
+
+// for (let i = 0; i <= 5; i += 1) {
+//   console.log(i);
+
+//   if (i === 3) {
+//     console.log("Нашли число 3, прерываем выполнение цикла");
+//     break;
+//   }
+// }
+
+// console.log("Лог после цикла");
+// Задание
+// Дополни код так, чтобы в переменную number записывалось первое число от start до end, которое делится на 5 без остатка.
+
+// Тесты
+// Объявлена переменная start со значением 6
+// Объявлена переменная end со значением 27
+// Объявлена переменная number без инициализации
+// Итоговое значение переменной number равно 10
+// В цикле for используется break для выхода до завершения всех итераций цикла
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+//   console.log(number);
+// }
+
+//============Задача 31 / 32============
+// Оператор break vs return в функции
+// Если цикл находится в теле функции, то оператор break не прекращает выполнение функции, а только прервёт цикл. Для того чтобы прерывать выполнение сразу цикла и функции есть оператор return.
+
+// В примере ищем число 3. Как только выполнится условие if, делаем возврат, который прервёт выполнение цикла и функции.
+
+// function fn() {
+//   for (let i = 0; i <= 5; i += 1) {
+//     console.log(i);
+
+//     if (i === 3) {
+//       console.log("Нашли число 3, делаем возврат, прерывая цикл и функцию");
+//       return i;
+//     }
+//   }
+
+//   // Этот console.log не выполнится
+//   console.log("Лог после цикла в теле функции");
+// }
+
+// const result = fn();
+// console.log("Лог после выхода из функции");
+// console.lof(`Результат выполнения функции ${result}`);
+// Задание
+// Выполни рефакторинг функции findNumber(start, end, divisor) так, чтобы она:
+
+// возвращала первое число от start до end, которое делится на divisor без остатка
+// не использовала оператор break
+// не использовала переменную number
+// Тесты
+// Объявлена функция findNumber(start, end, divisor)
+// Вызов findNumber(2, 6, 5) возвращает 5
+// Вызов findNumber(8, 17, 3) возвращает 9
+// Вызов findNumber(6, 9, 4) возвращает 8
+// Вызов findNumber(16, 35, 7) возвращает 21
+// Вызов findNumber() со случайным набором чисел возвращает верный результат
+// В цикле for не должен использоваться break для выхода до завершения всех итераций цикла
+
+// function findNumber(start, end, divisor) {
+//     // Change code below this line
+//     let number;
+
+//     for (let i = start; i < end; i += 1) {
+//       if (i % divisor === 0) {
+//   return i;
+//       }
+//     }
+
+//     return number;
+//     // Change code above this line
+//   }
+
+// Задача: функция includes()
+// Задание
+// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+// Тесты
+// Объявлена функция includes(array, value)
+// Вызов includes([1, 2, 3, 4, 5], 3) возвращает true
+// Вызов includes([1, 2, 3, 4, 5], 17) возвращает false
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") возвращает true
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") возвращает false
+// Вызов includes(["apple", "plum", "pear", "orange"], "plum") возвращает true
+// Вызов includes(["apple", "plum", "pear", "orange"], "kiwi") возвращает false
+// Вызов includes() для случайного массива со случайным value возвращает верный boolean
+// В функции includes используется for, return, но не метод массива includes
+
+// Задача: функция includes()
+// Задание
+// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+//============Задача 32 / 32============
+// Тесты
+// Объявлена функция includes(array, value)
+// Вызов includes([1, 2, 3, 4, 5], 3) возвращает true
+// Вызов includes([1, 2, 3, 4, 5], 17) возвращает false
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") возвращает true
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") возвращает false
+// Вызов includes(["apple", "plum", "pear", "orange"], "plum") возвращает true
+// Вызов includes(["apple", "plum", "pear", "orange"], "kiwi") возвращает false
+// Вызов includes() для случайного массива со случайным value возвращает верный boolean
+// В функции includes используется for, return, но не метод массива includes
+
+// function includes(array, value) {
+//     // Change code below this line
+
+//    for (let i = 0; i < array.length; i += 1){
+//        if (array[i] === value){
+//       return true;
+//       }
+//    }
+//   return false;
+//     // Change code above this line
+//   }
+
+//=============================================================
+
+// :pushpin: Очень важно не забывать о том, что наши методы работы с массивами как и условия, могут что-то возвращать.
+// :pushpin: Индексация в массиве начинается с 0.
+// :pushpin: С помощью  свойства length  мы можем получить длину массива или строки (возвращает число), будет полезно при вычислении последнего индекса массива.
+// :pushpin: Шпаргалка по методам массивов:
+// split/join – преобразует строку в массив и обратно.
+// push () – добавляет элементы в конец,
+// pop() – удаляет элемент с конца,
+// shift() – удаляет элемент с начала,
+// unshift() – добавляет элементы в начало.
+// splice(index, deleteIndex, ...arr) – начиная с индекса index, удаляет deleteIndex элементов и вставляет arr.
+// slice(start, end) – создаёт новый массив, копируя в него элементы с позиции start до end (не включая end).
+// :pushpin: Функция должна делать только то, что явно подразумевается её названием. И это должно быть одним действием. Если кратко и понятно: Одна функция – одно действие.
+// :pushpin: Шпаргалка по объявления функций в JS:
+// Функциональное выражение (function expression)
+// const greet = function (name) {
+//     return `Hello, ${name}`;
+// };
+// Объявление функции (function declaration)
+// function greet(name) {
+//     return `Hello, ${name}!`;
+// }
+// Стрелочная функция (arrow function)
+// const greet = who => return `Hello, ${name}!`;
+// :pushpin: Не воспринимайте стрелочную функцию, как что-то страшное. Первое, что надо понять - это укороченный вариант написания простой функции. Есть такое выражение - "синтаксический сахар"
+// :pushpin: Для того чтобы что-то вернуть из функции/метода или из условия, можно использовать оператор return. Так же из функции можно возвращать сразу любой типа данных, например
+// return 'hello' // (вернем строку)
+//   или
+// return true // (вернем буль)
+//  или
+// return [1, 2] // (вернем массив)
+// :pushpin:  Паттерн ранний возврат, говорит о том, что если условие внутри нашего if вернет true, то нам сразу же нужно что-то вернуть из тела этого условия, с помощью return.
+// :pushpin:  Не забывайте что оператор = это оператор присвоения, а не сравнения
+// :pushpin:  Несколько лайфхаков при работе с массивами
+// Как быстро очистить массив
+// const fruits = ['banana', 'apple', 'orange', 'watermelon', 'apple', 'orange', 'grape', 'apple'];
+// fruits.length = 0;
+// console.log(fruits); // вернет []
+// Как объединить более двух массивов
+// const fruits = ['apple', 'banana', 'orange'];
+// const meat = ['poultry', 'beef', 'fish'];
+// const vegetables = ['potato', 'tomato', 'cucumber'];
+// const food = [...fruits, ...meat, ...vegetables];
+// console.log(food); // вернет ["apple", "banana", "orange", "poultry", "beef", "fish", "potato", "tomato", "cucumber"]
+// Как получить рандомное значение массива
+// const fruits = [
+//     'banana',
+//     'apple',
+//     'orange',
+//     'watermelon',
+//     'apple',
+//     'orange',
+//     'grape',
+//     'apple',
+// ];
+// const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
+// console.log(randomFruit); // вернет рандомный фрукт из массива
